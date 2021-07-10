b6ed3644355405f32c199dc4c8ec995ea5638d4d
Elena Zabalueva <age_eva@mail.ru>
2021-07-10 14:04:38 +0300

    feat: add express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('dkfj')
+import express from 'express';
+
+const app = express();
+
+/* app.use(()) */
+app.get('/', (req, res) => {
+	res.send('Server')
+});
+app.listen(3000, () => console.log('Server start'));
